cd3a790d3feb4fe8d576e8f04afb94467479426b
Sagar Maheshwari <113180099+sagar-1m@users.noreply.github.com>
2024-08-04 17:04:18 +0530

    Time: 12 ms (33.7%), Space: 8.5 MB (46.75%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int dummy=x;
-        long long rev=0;
-        while(x>0){
-            rev=(rev*10)+x%10;
-            x=x/10;
+        // Negative numbers are not palindromes
+        // Numbers ending with 0 but not 0 itself are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        return rev==dummy;
         
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        
+        // When the length is odd, we can get rid of the middle digit by rev/10
+        return x == rev || x == rev / 10;
     }
 };
