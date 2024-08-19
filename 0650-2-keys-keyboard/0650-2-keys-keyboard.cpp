c13cc21eeacf90057ae2784681c1148bccce84e6
Sagar Maheshwari <113180099+sagar-1m@users.noreply.github.com>
2024-08-19 23:35:06 +0530

    Time: 0 ms (100%), Space: 7.5 MB (71.95%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int res = 0;
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                res += i;
+                n /= i;
+            }
+        }
+        if (n > 1) res += n;
+        return res;
+    }
+};
