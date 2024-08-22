0b662eafd9f00d645ca3256ffa99eb790a20e089
Sagar Maheshwari <113180099+sagar-1m@users.noreply.github.com>
2024-08-22 13:00:42 +0530

    Time: 3 ms (98.45%), Space: 14.4 MB (30.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
       unordered_map<int, int> seen;
       for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
-        if (seen.find(complement) != seen.end()) {
-            return {seen[complement], i};
+        int moreNeeded = target - nums[i];
+        if (seen.find(moreNeeded) != seen.end()) {
+            return {seen[moreNeeded], i};
             }
             seen[nums[i]] = i;
             }
