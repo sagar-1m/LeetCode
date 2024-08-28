9613208f2d57781da00c3451eb19a080a2875c4e
Sagar Maheshwari <113180099+sagar-1m@users.noreply.github.com>
2024-08-28 22:38:46 +0530

    Time: 13 ms (66.08%), Space: 20.3 MB (51.51%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1); // Start with 1 candy for each child
+
+        // Left to right pass
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Right to left pass
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Sum up the total candies
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
+    }
+};
